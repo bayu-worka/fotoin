1fb155e89b4158bb93f88d8906838be988cf41af
Muhammad Bayu Oktavianto <bayu@worka.id>
2018-02-09 11:28:34 +0700

    add comment gem

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ApplicationRecord
   acts_as_votable
+  acts_as_commentable
   mount_uploader :image, PhotoUploader
 
   belongs_to :user
---
+++ db/migrate/20180209040653_acts_as_commentable_with_threading_migration.rb
@@ -0,0 +1,21 @@
+class ActsAsCommentableWithThreadingMigration < ActiveRecord::Migration[5.1]
+  def self.up
+    create_table :comments, force: true, id: :uuid do |t|
+      t.uuid :commentable_id
+      t.string :commentable_type
+      t.string :title
+      t.text :body
+      t.string :subject
+      t.uuid :user_id, :null => false
+      t.uuid :parent_id, :lft, :rgt
+      t.timestamps
+    end
+
+    add_index :comments, :user_id
+    add_index :comments, [:commentable_id, :commentable_type]
+  end
+
+  def self.down
+    drop_table :comments
+  end
+end

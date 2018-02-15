e7b54a00eb21efd5b878173970b7855f2ba39c6a
Muhammad Bayu Oktavianto <bayu@worka.id>
2018-02-15 17:35:19 +0700

    get latest month & this month like

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,20 @@ class Photo < ApplicationRecord
 
   belongs_to :user
   validates_presence_of :image, :title
+
+  def get_latest_month_like
+    votes_for.where(created_at: Time.now.last_month.at_beginning_of_month..Time.now.last_month.end_of_month)
+  end
+
+  def get_this_month_like
+    votes_for.where(created_at: Time.now.at_beginning_of_month..Time.now.end_of_month)
+  end
+
+  def get_latest_month_like_size
+    get_latest_month_like.size
+  end
+
+  def get_this_month_like_size
+    get_this_month_like.size
+  end
 end

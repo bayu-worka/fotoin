130061a624078e676356a560a9552c6444f83769
Muhammad Bayu Oktavianto <bayu@worka.id>
2018-02-19 19:25:44 +0700

    add user id & user email

--- app/serializers/moment_serializer.rb
+++ app/serializers/moment_serializer.rb
@@ -1,5 +1,9 @@
 class MomentSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :user_id, :user_email
 
   has_many :photos, serializer: PhotoSerializer
+
+  def user_email
+    object.user.email
+  end
 end
